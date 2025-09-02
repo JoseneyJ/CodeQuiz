756b3598490ada707ea00fe11107575b3edac4dd
JoseneyJ <119374215+JoseneyJ@users.noreply.github.com>
2025-09-02 13:52:40 -0400

    Initial commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Candid Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Case Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answer: "A. Quotes",
+        options: [
+            "A. Quotes",
+            "B. Commas",
+            "C. Curly Brackets",
+            "D. Parentheses"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "D. Console.log",
+        options: [
+            "A. Terminal/Bash",
+            "B. Javascript",
+            "C. For loops",
+            "D. Console.log"
+        ]
+    }
+];
